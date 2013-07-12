6fd892ad5ac20d39eef526c404de721b5123e8eb
Martin Samson <pyrolian@gmail.com>
2013-07-12 14:04:54 -0400

    Update config.py
    Updated scopes.

--- flask_trois/config.py
+++ flask_trois/config.py
@@ -3,5 +3,5 @@ from shopify_trois import Credentials
 SHOPIFY_CREDENTIALS = Credentials(
     api_key='your api key',
     secret='your api key secret',
-    scope=frozenset(['write_blogs', 'read_order'])
+    scope=frozenset(['write_content', 'read_orders'])
 )
